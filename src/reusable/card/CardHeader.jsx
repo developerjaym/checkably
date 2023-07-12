5aa4a367f9ad3c72e6466101a8077ee6c71652f1
Jay M <jaymansmann@yahoo.com>
2023-07-12 16:38:09 -0400

    fixes icon--header flicker

--- src/reusable/card/CardHeader.jsx
+++ src/reusable/card/CardHeader.jsx
@@ -3,7 +3,6 @@ import "./CardHeader.css"
 
 export default function CardHeader({title, icon}) {
     return (<header className="card__header">
-        {/* {icon ? <img className="header__img" src={icon.src} alt={icon.alt} width={32} height={32}/> : null} */}
         {icon ? <Icon icon={icon} className="icon--header"/> : null}
         <h2>{title}</h2>
     </header>)
