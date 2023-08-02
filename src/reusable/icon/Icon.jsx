74842730dbdfaa633eaf89a148ce06cf7e8a6f8f
Jay M <jaymansmann@yahoo.com>
2023-08-02 13:55:41 -0400

    Giving svg height and width properties for safari

--- src/reusable/icon/Icon.jsx
+++ src/reusable/icon/Icon.jsx
@@ -3,10 +3,19 @@ import "./Icon.css";
 
 function IconComponent({icon, className = "button__icon"}) {
     const [svg, setSVG] = useState("");
+    const cleanupSVG = (svgData) => {
+        if(!svgData.includes("width")) {
+            svgData = svgData.replace("svg", `svg width="24" `)
+        }
+        if(!svgData.includes("height")) {
+            svgData = svgData.replace("svg", `svg height="24" `)
+        }
+        return svgData
+    }
     useEffect(() => {
         fetch(icon.src)
         .then(resp => resp.text())
-        .then(svgData => setSVG(svgData))
+        .then(svgData => setSVG(cleanupSVG(svgData)))
     }, [icon])
     return (<div aria-description={icon.alt} role="img" className={`icon ${className}`} dangerouslySetInnerHTML={{__html: svg}}/>)
 }
